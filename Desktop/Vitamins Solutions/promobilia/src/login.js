dc69ff1786dcdc7724b261c27451305568d66657
aandres44 <chuy27arts@gmail.com>
2021-03-07 14:17:04 -0600

    subiendo login y register

---
+++ Desktop/Vitamins Solutions/promobilia/src/login.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import withFirebaseAuth from 'react-with-firebase-auth';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+import firebaseConfig from './firebase';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+class Login extends Component {
+    render() {
+        const {user, signOut, signInWithGoogle} = this.props;
+        return (
+            <div>
+                {
+                    user ?
+                    <p>Hola, {user.displayName}</p>
+                    : <p>Porfavor, inicia sesión</p>
+                }
+                {
+                    user 
+                    ? <button onClick={signOut}>Cerrar sesión</button>
+                    : <button onClick={signInWithGoogle}>Iniciar sesión</button>
+                }
+            </div>
+        );
+    }
+}
+
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+    googleProvider : new firebase.auth.GoogleAuthProvider(),
+};
+
+export default withFirebaseAuth({
+    providers,
+    firebaseAppAuth,
+})(Login);
+
